0d3c4c1f3d85cd37e58210d44bec8828cfa1899a
isabela-dominguez <15iidh@queensu.ca>
2020-11-22 22:12:20 -0500

    acceleration process, fourier to get noise

--- data_visualization.py
+++ data_visualization.py
@@ -1,7 +1,8 @@
 from config import * 
 
+
 # import data from CSV
-df = pd.read_csv('data/ziads_data/Walking.csv', engine="python",index_col=False, sep=";", header= 1, encoding='utf-8' )
+df = pd.read_csv('data/ziads_data/Jumping.csv', engine="python",index_col=False, sep=";", header= 1, encoding='utf-8' )
 
 print(df.size)
 print(df.dtypes)
@@ -10,4 +11,117 @@ print(df.dtypes)
 print(df)
 # Take a look at all sensor outputs
 df.plot(subplots=True,sharex=True,layout=(6,6))
+plt.show()
+
+dt = 0.01 # check at what frequency we are sampling
+
+
+# transfrom x,y,z accelerations. 
+# we The phone’s accelerometer measures acceleration in reference to itself (the body frame) and we need to transform the body frame accelerations to an inertial, non-rotating frame ( the earth frame)
+#  This will make it possible to hold the phone in any orientation, and measure the correct acceleration vectors to calculate the phone’s trajectory in the earth frame.
+
+# Convert orientation measurement units to radians
+cols_angles = ['ORIENTATION X (pitch °)','ORIENTATION Y (roll °)',
+               'ORIENTATION Z (azimuth °)']
+for axis in cols_angles:
+    df[axis] = df[axis] * pi/180
+# Transform body frame accelerations into the inertial (Earth) frame
+    # Set up rotation matrices
+def R_x(x):
+    # body frame rotation about x axis
+    return np.array([[1,      0,       0],
+                     [0,cos(-x),-sin(-x)],
+                     [0,sin(-x), cos(-x)]])
+def R_y(y):
+    # body frame rotation about y axis
+    return np.array([[cos(-y),0,-sin(-y)],
+                    [0,      1,        0],
+                    [sin(-y), 0, cos(-y)]])
+def R_z(z):
+    # body frame rotation about z axis
+    return np.array([[cos(-z),-sin(-z),0],
+                     [sin(-z), cos(-z),0],
+                     [0,      0,       1]])
+# Set up arrays to hold acceleration data for transformation
+accel = np.array([df['ACCELEROMETER X (m/s²)'],
+                  df['ACCELEROMETER Y (m/s²)'],
+                  df['ACCELEROMETER Z (m/s²)']])
+grav = np.array([df['GRAVITY X (m/s²)'],
+                 df['GRAVITY Y (m/s²)'],
+                 df['GRAVITY Z (m/s²)']])
+line = np.array([df['LINEAR ACCELERATION X (m/s²)'],
+                 df['LINEAR ACCELERATION Y (m/s²)'],
+                 df['LINEAR ACCELERATION Z (m/s²)']])
+# Set up arrays to hold Euler angles for rotation matrices
+pitch = df['ORIENTATION X (pitch °)']
+roll = df['ORIENTATION Y (roll °)']
+yaw = df['ORIENTATION Z (azimuth °)']
+# Initilize arrays for new transformed accelerations
+earth_accels = np.empty(accel.shape)
+earth_gravity = np.empty(accel.shape)
+earth_linear = np.empty(accel.shape)
+# Perform frame transformations (body frame --> earth frame) 
+for i in range(df.shape[0]):
+    # accel_earth = (RzRyRx)(accel_body)
+    earth_accels[:,i] = R_z(yaw[i]) @ R_y(roll[i]) @ R_x(pitch[i]) @ accel[:,i]
+    earth_gravity[:,i] = R_z(yaw[i]) @ R_y(roll[i]) @ R_x(pitch[i]) @ grav[:,i]
+    earth_linear[:,i] = R_z(yaw[i]) @ R_y(roll[i]) @ R_x(pitch[i]) @ line[:,i]
+# Create new columns in dataframe for earth frame accelerations    
+df['EARTH ACCELERATION X'] = earth_accels[0,:]
+df['EARTH ACCELERATION Y'] = earth_accels[1,:]
+df['EARTH ACCELERATION Z'] = earth_accels[2,:]
+df['EARTH GRAVITY X'] = earth_gravity[0,:]
+df['EARTH GRAVITY Y'] = earth_gravity[1,:]
+df['EARTH GRAVITY Z'] = earth_gravity[2,:]
+df['EARTH LINEAR ACCELERATION X'] = earth_linear[0,:]
+df['EARTH LINEAR ACCELERATION Y'] = earth_linear[1,:]
+df['EARTH LINEAR ACCELERATION Z'] = earth_linear[2,:]
+
+
+# Plot new accelerations
+cols_earth = ['EARTH ACCELERATION X','EARTH ACCELERATION Y',
+              'EARTH ACCELERATION Z','EARTH GRAVITY X','EARTH GRAVITY Y','EARTH GRAVITY Z','EARTH LINEAR ACCELERATION X','EARTH LINEAR ACCELERATION Y','EARTH LINEAR ACCELERATION Z']
+cols_body = ['ACCELEROMETER X (m/s²)','ACCELEROMETER Y (m/s²)',
+             'ACCELEROMETER Z (m/s²)', 'GRAVITY X (m/s²)', 'GRAVITY Y (m/s²)','GRAVITY Z (m/s²)', 'LINEAR ACCELERATION X (m/s²)',
+ 'LINEAR ACCELERATION Y (m/s²)', 'LINEAR ACCELERATION Z (m/s²)']
+bodyplot = df.plot(y=cols_body,subplots=True,sharex=True,layout=(3,3),style='k',alpha=0.5,title=cols_body)
+df.plot(y=cols_earth,subplots=True,layout=(3,3),ax=bodyplot,
+sharex=True,style='g',
+title='Body Frame to Earth Frame Accelerations')
+plt.show()
+
+
+# Double integrating the earth fram acceleration 
+x =cumtrapz(cumtrapz(df['EARTH LINEAR ACCELERATION X'],dx=dt),dx=dt)
+y =cumtrapz(cumtrapz(df['EARTH LINEAR ACCELERATION Y'],dx=dt),dx=dt)
+z =cumtrapz(cumtrapz(df['EARTH LINEAR ACCELERATION Z'],dx=dt),dx=dt)
+# Plot 3D Trajectory
+fig3,ax = plt.subplots()
+fig3.suptitle('3D Trajectory of phone',fontsize=20)
+ax = plt.axes(projection='3d')
+ax.plot3D(x,y,z,c='red',lw=5,label='phone trajectory')
+ax.set_xlabel('X position (m)')
+ax.set_ylabel('Y position (m)')
+ax.set_zlabel('Z position (m)')
+plt.show()
+
+
+
+# Try to remove noise via Fourier analysis
+    # Discrete Fourier Transform sample frequencies
+freq = np.fft.rfftfreq(df['EARTH LINEAR ACCELERATION X'].size,d=dt)
+# Compute the Fast Fourier Transform (FFT) of acceleration signals
+fft_x = np.fft.rfft(df['EARTH LINEAR ACCELERATION X']) 
+fft_y = np.fft.rfft(df['EARTH LINEAR ACCELERATION Y']) 
+fft_z = np.fft.rfft(df['EARTH LINEAR ACCELERATION Z'])
+# Plot Frequency spectrum
+fig4,[ax1,ax2,ax3] = plt.subplots(3,1,sharex=True,sharey=True)
+fig4.suptitle('Noise Spectrum',fontsize=20)
+ax1.plot(freq,abs(fft_x),c='r',label='x noise')
+ax1.legend()
+ax2.plot(freq,abs(fft_y),c='b',label='y noise')
+ax2.legend()
+ax3.plot(freq,abs(fft_z),c='g',label='z noise')
+ax3.legend()
+ax3.set_xlabel('Freqeuncy (Hz)')
 plt.show()
